0fab47f14526d421c720316b3b2c252364ce368c
DeerEdge <vislawath.21@gmail.com>
2023-08-06 21:20:31 -0600

    bootstrap

--- Home.py
+++ Home.py
@@ -1,19 +1,11 @@
-from flask import Flask, redirect, url_for, app
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello!"
+    return render_template("index.html")
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    app.run()
-
+    app.run(debug=True)
